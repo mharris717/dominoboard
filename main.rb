2c7905c07e276d65610e5b554eca26d4f3aba327
Mike Harris <mharris@indians.com>
2012-01-18 18:06:33 -0500

    cs

--- main.rb
+++ main.rb
@@ -1,5 +1,172 @@
 require 'nokogiri'
 require 'open-uri'
+require 'mharris_ext'
+require 'andand'
+
+gem 'mongoid','2.0.1'
+require 'mongoid'
+
+class String
+  def rpad(n)
+    return self if length >= n
+    pad = " "*(n-length)
+    "#{self}#{pad}"
+  end
+end
+
+Mongoid.configure do |config|
+  name = "dominoboard_dev"
+  host = "localhost"
+  config.master = Mongo::Connection.new(host).db(name)
+
+  config.persist_in_safe_mode = false
+end
+
+class Ranking
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :skill, :type => Float
+  field :stdev, :type => Float
+  field :username
+  field :rank, :type => Fixnum
+  field :games_played, :type => Fixnum
+  field :batch_dt, :type => Time
+  def rating_str
+    "#{skill} +- #{stdev}"
+  end
+  def self.create_from_row!(row,batch_dt)
+    res = new(:batch_dt => batch_dt)
+    tds = row.xpath("td").map { |x| x.text.strip }
+    raise "bad tds size #{tds.size} #{row.text}" unless tds.size == 5
+    res.skill, res.stdev = *tds[1].split(" ").reject { |x| x.length < 3 }.map { |x| x.to_f }
+    res.rank, res.games_played, res.username = *tds[2..4]
+    res.save!
+    res
+  end
+end
+
+class LatestRanking
+  class << self
+    fattr(:instance) { new }
+    def method_missing(sym,*args,&b)
+      instance.send(sym,*args,&b)
+    end
+  end
+
+  fattr(:batch_dt) do
+    Ranking.order_by([:batch_dt, :desc]).limit(1).first.batch_dt
+  end
+  fattr(:cached_rankings) do
+    Ranking.where(:batch_dt => batch_dt)
+  end
+  def rankings
+    #batch_dt!
+    #cached_rankings! if cached_rankings.first.batch_dt != batch_dt
+    cached_rankings
+  end
+  fattr(:ranking_hash) do
+    res = {}
+    rankings.each do |r|
+      res[r.username] = r.rating_str
+    end
+    res
+  end
+  def get_rating(username)
+    #rankings.select { |x| x.username == username }.first.andand.skill
+    ranking_hash[username]
+  end
+end
+
+class Game
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :game_id
+  field :winner
+  field :loser
+  field :players, :type => Array
+  field :game_dt, :type => Time
+  field :game_log
+  #game-20120117-200600-93875d32
+  def url
+    "http://councilroom.com/game?game_id=#{game_id}.html"
+  end
+  def fill_from_remote!
+    self.game_log = open(url).read if self.game_log.blank?
+    doc = Nokogiri::HTML(game_log)
+    bolds = doc.xpath("//b")
+    self.winner = bolds.find { |x| x.text =~ /#1 / }.text[3..-1]
+    self.loser = bolds.find { |x| x.text =~ /#2 / }.text[3..-1]
+    self.players = [winner,loser]
+    save!
+  rescue => exp
+    puts "error #{exp.message}"
+  end
+  def fill_game_dt!
+    raise "bad" unless game_id =~ /game-(\d{4})(\d{2})(\d{2})-(\d{2})(\d{2})/
+    args = [$1,$2,$3,$4,$5].map { |x| x.to_i }
+    self.game_dt = Time.local(*args)
+  end
+  before_create "fill_game_dt!"
+  class << self
+    def add!(game_id)
+      game_id = game_id.strip
+      create!(:game_id => game_id) if where(:game_id => game_id).count == 0
+    end
+    def add_player!(username)
+
+    end
+  end
+end
+
+class Player
+  include FromHash
+  attr_accessor :username
+  def url
+    "http://councilroom.com/search_result?p1_name=#{username}"
+  end
+  def add_games!
+    doc = Nokogiri::HTML(open(url))
+    links = doc.xpath("//a").map { |x| x.get_attribute('href') }.select { |x| x =~ /game\?game_id/i }
+    links.each do |link|
+      raise "unknown #{link}" unless link =~ /game\?game_id=(.*)\.html/i
+      Game.add! $1
+    end
+  end
+  def games
+    Game.where(:players => username).sort_by { |x| x.game_dt }
+  end
+  def to_s_history
+    games.map do |g|
+      opp = (g.players - ['mharris717']).first
+      rating = LatestRanking.get_rating(opp)
+      won = (g.winner == username) ? "Won " : "Lost"
+      dt = g.game_dt.strftime("%m/%d")
+      "#{dt} #{won} vs #{opp.rpad(20)} #{rating}"
+    end.join("\n")
+  end
+
+  class << self
+    def add_games!(username)
+      new(:username => username).add_games!
+    end
+  end
+end
+
+
+def log_table
+  $str = open("http://dominion.isotropic.org/leaderboard")
+  doc = Nokogiri::HTML($str)
+  puts 'getting rows'
+  rows = doc.xpath("//tr")
+  puts rows.size
+
+  #rows = rows.select { |x| x.text =~ /(mharris717|notadam|bossbri|lawnboy)/i }
+  rows = rows.select { |x| x.text =~ /\d\.\d+/i }
+  batch_dt = Time.now
+  rows.each do |row|
+    Ranking.create_from_row!(row,batch_dt)
+  end
+end
 
 def get_table
   $str = open("http://dominion.isotropic.org/leaderboard")
@@ -24,11 +191,38 @@ def get_table
   res
 end
 
-require 'sinatra'
+if false
+  require 'sinatra'
 
-get "/" do
-  get_table
+  get "/" do
+    get_table
+  end
 end
 
+#log_table
+puts Ranking.count
 
 
+
+puts LatestRanking.batch_dt
+puts LatestRanking.rankings.size
+
+#Game.destroy_all
+
+if false
+  Game.add!("game-20120117-193312-3b4edf81")
+  g = Game.first
+  g.fill_from_remote!
+  puts g.players.inspect
+end
+
+#p = Player.add_games!(:mharris717)
+
+#puts Game.count
+#puts Game.last.game_dt
+
+puts Game.all.select { |x| x.winner }.size
+puts Game.all.reject { |x| x.winner }.size
+
+puts LatestRanking.get_rating('NotAdam')
+File.create("games.txt",Player.new(:username => 'mharris717').to_s_history)
